255df198fbcca2068f851d30425edd10d4632cd9
darknesslord19 <admin.lord19@gmail.com>
2026-09-25 23:46:30 +0300

    Add darkneslord-cloudstream/nik-cloudstream-master/DiziPalOriginal/build.gradle.kts (ZIP upload)

---
+++ darkneslord-cloudstream/nik-cloudstream-master/DiziPalOriginal/build.gradle.kts
@@ -0,0 +1,19 @@
+version = 92
+
+cloudstream {
+    authors = listOf("csprofesor")
+    language    = "tr"
+    description = "en yeni dizileri güvenli ve hızlı şekilde sunar."
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("TvSeries", "Movie")
+    iconUrl = "https://chessplyimages.cfd/uploads/site/favicon-32x32.png"
+    requiresResources = false
+}
