7a760ceb70bd5fc2b3e5aed7c6e5a234ffd394e9
darknesslord19 <admin.lord19@gmail.com>
2026-09-25 23:49:13 +0300

    Add darkneslord-cloudstream/nik-cloudstream-master/HintFilmIzle/build.gradle.kts (ZIP upload)

---
+++ darkneslord-cloudstream/nik-cloudstream-master/HintFilmIzle/build.gradle.kts
@@ -0,0 +1,13 @@
+version = 41
+
+cloudstream {
+    authors = listOf("csprofesor")
+    language = "tr"
+    description = "HintFilmizle.com Hint filmleri, yabancı filmler ve diziler"
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    iconUrl = "https://www.google.com/s2/favicons?domain=www.hintfilmizle.com&sz=%size%"
+}
+
+// Published in the main repository plugin list
+// Build trigger
