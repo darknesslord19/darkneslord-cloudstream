2bcd859cf380cb04b4ea9be8fc43cca2de6f9c5d
darknesslord19 <admin.lord19@gmail.com>
2026-09-25 23:43:45 +0300

    Add darkneslord-cloudstream/nik-cloudstream-master/Anizm/build.gradle.kts (ZIP upload)

---
+++ darkneslord-cloudstream/nik-cloudstream-master/Anizm/build.gradle.kts
@@ -0,0 +1,13 @@
+version = 10
+
+cloudstream {
+    language = "tr"
+    authors = listOf("csprofesor")
+    status = 1
+    tvTypes = listOf(
+        "AnimeMovie",
+        "Anime",
+        "OVA",
+    )
+    iconUrl = "https://www.google.com/s2/favicons?domain=anizm.com.tr&sz=%size%"
+}
