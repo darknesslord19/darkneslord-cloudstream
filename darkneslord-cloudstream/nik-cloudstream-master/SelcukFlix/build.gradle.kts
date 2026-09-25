ed5cae2860ea966c759d8038a1dfc36f9bc07105
darknesslord19 <admin.lord19@gmail.com>
2026-09-25 23:50:59 +0300

    Add darkneslord-cloudstream/nik-cloudstream-master/SelcukFlix/build.gradle.kts (ZIP upload)

---
+++ darkneslord-cloudstream/nik-cloudstream-master/SelcukFlix/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 2
+
+cloudstream {
+    authors = listOf("csprofesor")
+    language    = "tr"
+    description = "SelcukFlix eklentisi."
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("Movie", "TvSeries")
+    iconUrl = "https://selcukflix.co/assets/favicon/favicon-32x32.png"
+}
