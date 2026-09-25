f63b14bb9de8d972cf9aedfabdd05ffd6f026f39
darknesslord19 <admin.lord19@gmail.com>
2026-09-25 23:45:48 +0300

    Add darkneslord-cloudstream/nik-cloudstream-master/DiziMag/build.gradle.kts (ZIP upload)

---
+++ darkneslord-cloudstream/nik-cloudstream-master/DiziMag/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 22
+
+cloudstream {
+    authors = listOf("csprofesor")
+    language    = "tr"
+    description = "En son çıkan yabancı dizi ve filmleri DiziMag farkıyla full hd 1080p kalitede izle. Dizimag Geniş ve güncel arşiviyle dizi ve filmlerin tadını çıkartın."
+    status  = 1
+    tvTypes = listOf("TvSeries", "Movie")
+    iconUrl = "https://www.google.com/s2/favicons?domain=dizimag.one&sz=%size%"
+}
